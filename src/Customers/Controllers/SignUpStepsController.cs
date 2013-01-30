89752361d640e82452128047e6fb7ca35f5e4228
Vlad <Vladyslav.Alieksieienko@n-cube.co.uk>
2013-01-30 13:39:00 +0200

    Tried knockout template

---
+++ src/Customers/Controllers/SignUpStepsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Customers.Controllers
+{
+    public class SignUpStepsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
